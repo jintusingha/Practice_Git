5130504894039b1f18eb7a5799630224e65336f0
jintu singha <jintumonisingha411@gmail.com>
2025-08-09 13:09:59 +0530

    First Commit

---
+++ app/src/main/java/com/jintu/practicegit/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.jintu.practicegit
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jintu.practicegit.ui.theme.PracticegitTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PracticegitTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello user $name!",
+
+        modifier = modifier
+    )
+    Text(
+        text = "here we go again folk's "
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PracticegitTheme {
+        Greeting("Android")
+    }
+}
